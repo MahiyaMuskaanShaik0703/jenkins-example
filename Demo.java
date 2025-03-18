5b6a9f921bdfbb990183145aa4050223e8cdcc2b
MahiyaMuskaanShaik0703 <mahiyamuskaan2005@gmail.com>
2025-03-18 15:00:34 +0530

    commit

---
+++ Demo.java
@@ -0,0 +1,13 @@
+class Demo{
+<<<<<<< HEAD
+	public static void main(String args[]){
+		System.out.println("Hello World");
+	}
+}
+=======
+        public static void main(String args[]){
+                System.out.println("Hello World");
+        }
+}
+
+>>>>>>> 71bf0b2 (commit)
