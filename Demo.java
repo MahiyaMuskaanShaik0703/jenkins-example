43fd2db39f2307d31ede27a74534756021c4272d
Mahiya Muskaan Shaik <mahiyamuskaan2005@gmail.com>
2025-03-18 15:55:44 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,13 +1,6 @@
 class Demo{
-<<<<<<< HEAD
+
 	public static void main(String args[]){
 		System.out.println("Hello World");
 	}
 }
-=======
-        public static void main(String args[]){
-                System.out.println("Hello World");
-        }
-}
-
->>>>>>> 71bf0b2 (commit)
